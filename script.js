b200445e2260642f24d8fa71a45b7950c439f564
Abiolayoung <abiolayoung229@gmail.com>
2024-02-08 00:00:32 +0100

    yarn

---
+++ script.js
@@ -0,0 +1,11 @@
+// NPM is the first package manager to be released and it has been in existence for over 10 years
+/* Differences between package managers includes
+* dependecies resolution algo for node_modules: nested vs flat, node_modules vs PnP
+* Lock file formats
+* Package storage
+* monorepo support
+* flexibilty degree
+
+YARN means- yet another resource negotiatior and was released in 2016
+
+PNPM- released in 2017
